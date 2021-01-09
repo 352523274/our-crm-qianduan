12dbc0197ab22a4bbdc60869a5296b609e5bafe2
a876899548@163.com <876899548@qq.com>
2021-01-09 17:20:37 +0800

    大帅逼的第一次上传

---
+++ src/views/SellOrderDetail/index.js
@@ -0,0 +1,41 @@
+import sellOrderDetail from "@/api/sellOderDetail";
+
+export default {
+    name: "index.vue",
+    data() {
+
+        return {
+             tableData: [],
+            table1Data:[],
+             sellId:0,
+            table2Data:[],
+        }
+
+
+    },
+    created() {
+        this .finaAll();
+    },
+    methods:{
+       async finaAll(){
+
+           let params = this.$route.params.param;
+
+
+           this.sellId=params
+           let respone= await sellOrderDetail.findById(this.sellId)
+           this.consigneeId=respone.sellOrder.consigneeId
+           let respons = await  sellOrderDetail.findDetailById(this.consigneeId)
+           console.log(respons);
+           this.tableData.push(respone)
+           this.table1Data.push(respons[0])
+           this.table2Data=respons
+       },
+
+
+
+
+
+
+    }
+}
